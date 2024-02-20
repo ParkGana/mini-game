8901a6d2223bf0c9aae3744e89994b3e9076db38
ParkGana <gnchoco97@gmail.com>
2024-02-20 12:07:53 +0900

    example 관련 코드 및 파일 제거

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-import { Example } from '@/src/page/example/example.view'
-
 export default function Page() {
-    return <Example />
+    return <>INDEX</>
 }
--- redux/store.ts
+++ redux/store.ts
@@ -1,8 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import exampleReducer from './slice/example-slice'
 
 export const store = configureStore({
-    reducer: {
-        example: exampleReducer
-    }
+    reducer: {}
 })
